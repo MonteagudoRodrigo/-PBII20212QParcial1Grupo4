6c45539d9ce6ea15d0061f2cb674d41d11f04b8a
HernanAlbornoz <hernanalbornozjdg@gmail.com>
2021-10-13 01:09:46 -0300

    Peso Argentino caracteriscas

--- src/ar/edu/unlam/PBII20212QParcial1Grupo4/Banco.java
+++ src/ar/edu/unlam/PBII20212QParcial1Grupo4/Banco.java
@@ -1,5 +1,6 @@
 package ar.edu.unlam.PBII20212QParcial1Grupo4;
 
 public class Banco {
-
+	public Billetera billeteras[];
+	
 }
--- src/ar/edu/unlam/PBII20212QParcial1Grupo4/PesoArgentino.java
+++ src/ar/edu/unlam/PBII20212QParcial1Grupo4/PesoArgentino.java
@@ -1,5 +1,25 @@
 package ar.edu.unlam.PBII20212QParcial1Grupo4;
 
-public class PesoArgentino {
+public class PesoArgentino extends Cuenta{
 	//TODO: Hernán Albornoz
+	
+	private final static Double MINIMO_DEPOSITO_PARA_INTERES=10000.00; 
+	private final static Double INTERES=1.03;
+	
+	public PesoArgentino(Double saldo) {
+		super(saldo);
+		// TODO Auto-generated constructor stub
+	}
+	
+	@Override
+	public void depositar(Double monto) {
+		Double resultadoMonto=monto;
+		if(monto>=MINIMO_DEPOSITO_PARA_INTERES) {
+			resultadoMonto*=INTERES;
+		}
+		super.depositar(resultadoMonto);
+	}
+
+	
+	
 }
