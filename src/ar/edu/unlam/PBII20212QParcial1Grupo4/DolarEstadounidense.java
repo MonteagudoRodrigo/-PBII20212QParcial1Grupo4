292e3fbb085c942ae5c20fa39740e42c0d6d728a
MonteagudoRodrigo <71356504+MonteagudoRodrigo@users.noreply.github.com>
2021-10-13 03:24:27 -0300

    agegue compras a dolar

--- src/ar/edu/unlam/PBII20212QParcial1Grupo4/DolarEstadounidense.java
+++ src/ar/edu/unlam/PBII20212QParcial1Grupo4/DolarEstadounidense.java
@@ -1,6 +1,13 @@
 package ar.edu.unlam.PBII20212QParcial1Grupo4;
 
-public class DolarEstadounidense extends Cuenta implements Compra,Venta{
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.util.Date;
+
+public class DolarEstadounidense extends Cuenta implements Compra, Venta {
+	private final static Double INTERES = 1.05;
+	private final static Double LIMITE_COMPRA = 1000.0;
+	private Integer mesUltimaCompra;
 
 	public DolarEstadounidense(Double saldo) {
 		super(saldo);
@@ -9,12 +16,9 @@ public class DolarEstadounidense extends Cuenta implements Compra,Venta{
 
 	@Override
 	public void depositar(Double monto) {
-if(monto >= 1) {
-	saldo += (monto *1.05);
-}else {
-	super.depositar(monto);
-}
-		
+
+		super.depositar(monto * INTERES);
+
 	}
 
 	@Override
@@ -22,21 +26,32 @@ if(monto >= 1) {
 		// TODO Auto-generated method stub
 		return super.extraer(monto);
 	}
-	
-	@Override
-	public Boolean vender(Double monto) {
+
+	public Boolean vender(Cuenta cuentaCredito, Double montoAVender) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Boolean comprar(Cuenta cuentaDebito, Double montoAComprar) {
+		if (montoAComprar > LIMITE_COMPRA || montoAComprar <= 0) {
+			return false;
+		}
 		
-		// TODO Auto-generated method stub
+		Date date = new Date();
+		LocalDate localDate = date.toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
+		Integer month = localDate.getMonthValue();
+		
+		
+		if (cuentaDebito.getSaldo() >= montoAComprar * cuentaDebito.cotizacion) {
+			if (this.mesUltimaCompra > 0)
+
+				this.mesUltimaCompra = month;
+			cuentaDebito.extraer(montoAComprar * cuentaDebito.cotizacion);
+			super.depositar(montoAComprar);
+			return true;
+		}
+
 		return false;
 	}
-	@Override
-	public Boolean comprar(Double monto) {
-		// TODO Auto-generated method stub
-		return false;
-	}
-	
-	
-public class DolarEstadounidense {
-	//TODO: Rodrigo Monteagudo
-	// 		EN EL CONTRUCTOR: this.cotizacionSobreDolar = 1.0;
+
 }
