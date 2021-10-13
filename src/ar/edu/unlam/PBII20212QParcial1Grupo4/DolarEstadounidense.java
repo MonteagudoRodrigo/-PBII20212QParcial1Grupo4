02158be69dd2cc4eb25b7dcf9d603d3f852bdcf2
MonteagudoRodrigo <71356504+MonteagudoRodrigo@users.noreply.github.com>
2021-10-13 02:13:11 -0300

    crear clase dollar
    crear clase dollar

--- src/ar/edu/unlam/PBII20212QParcial1Grupo4/DolarEstadounidense.java
+++ src/ar/edu/unlam/PBII20212QParcial1Grupo4/DolarEstadounidense.java
@@ -1,5 +1,39 @@
 package ar.edu.unlam.PBII20212QParcial1Grupo4;
 
-public class DolarEstadounidense {
-	//TODO: Rodrigo Monteagudo
+public class DolarEstadounidense extends Cuenta implements Compra,Venta{
+
+	public DolarEstadounidense(Double saldo) {
+		super(saldo);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void depositar(Double monto) {
+if(monto >= 1) {
+	saldo += (monto *1.05);
+}else {
+	super.depositar(monto);
+}
+		
+	}
+
+	@Override
+	public Boolean extraer(Double monto) {
+		// TODO Auto-generated method stub
+		return super.extraer(monto);
+	}
+	
+	@Override
+	public Boolean vender(Double monto) {
+		
+		// TODO Auto-generated method stub
+		return false;
+	}
+	@Override
+	public Boolean comprar(Double monto) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+	
+	
 }
