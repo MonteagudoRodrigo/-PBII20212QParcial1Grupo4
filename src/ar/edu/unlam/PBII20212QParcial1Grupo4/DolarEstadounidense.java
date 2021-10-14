d5171254290cb5bccce7b39074c3bf1c5ed409b9
MonteagudoRodrigo <71356504+MonteagudoRodrigo@users.noreply.github.com>
2021-10-13 23:32:05 -0300

    terminados compra y venta

--- src/ar/edu/unlam/PBII20212QParcial1Grupo4/DolarEstadounidense.java
+++ src/ar/edu/unlam/PBII20212QParcial1Grupo4/DolarEstadounidense.java
@@ -8,9 +8,11 @@ public class DolarEstadounidense extends Cuenta implements Compra, Venta {
 	private final static Double INTERES = 1.05;
 	private final static Double LIMITE_COMPRA = 1000.0;
 	private Integer mesUltimaCompra;
+	private Double saldoParcial;
 
 	public DolarEstadounidense(Double saldo) {
 		super(saldo);
+		this.saldoParcial = 0.00;
 		// TODO Auto-generated constructor stub
 	}
 
@@ -27,32 +29,42 @@ public class DolarEstadounidense extends Cuenta implements Compra, Venta {
 		return super.extraer(monto);
 	}
 
-	public Boolean vender(Cuenta cuentaCredito, Double montoAVender) {
-		if(montoAVender <= saldo && montoAVender != null && montoAVender > 0) {
-			super.extraer(montoAVender);
-			cuentaCredito.depositar(montoAVender);
+	public Boolean vender(PesoArgentino cuentaCredito, Double montoAVender) {
+		if (montoAVender <= saldo && montoAVender > 0) {
+			this.extraer(montoAVender);
+			cuentaCredito.depositar(montoAVender * cuentaCredito.getCotizacionDolar());
+
 			return true;
 		}
 		return false;
 	}
 
-	public Boolean comprar(Cuenta cuentaDebito, Double montoAComprar) {
+	public Boolean comprar(PesoArgentino cuentaDebito, Double montoAComprar) {
+		Boolean compraValida = false;
 		if (montoAComprar > LIMITE_COMPRA || montoAComprar <= 0) {
+			
 			return false;
 		}
-		
+
 		Date date = new Date();
 		LocalDate localDate = date.toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
 		Integer month = localDate.getMonthValue();
-		
-		
-		if (cuentaDebito.getSaldo() >= montoAComprar * cuentaDebito.cotizacion) {
-			if (this.mesUltimaCompra > 0)
 
+		if (cuentaDebito.getSaldo() >= montoAComprar * cuentaDebito.getCotizacionDolar()) {
+			if (this.mesUltimaCompra == 0 || this.mesUltimaCompra > 0 && this.mesUltimaCompra != month) {
 				this.mesUltimaCompra = month;
-			cuentaDebito.extraer(montoAComprar * cuentaDebito.cotizacion);
+				saldoParcial = montoAComprar;
+				compraValida = true;
+				
+			} else if (this.mesUltimaCompra > 0 && this.mesUltimaCompra == month && (saldoParcial+montoAComprar)< 1000) {
+				saldoParcial += montoAComprar;
+				compraValida = true;
+			}
+			if(compraValida) {
+			cuentaDebito.extraer(montoAComprar * cuentaDebito.getCotizacionDolar());
 			super.depositar(montoAComprar);
 			return true;
+			}
 		}
 
 		return false;
--- src/ar/edu/unlam/PBII20212QParcial1Grupo4/PesoArgentino.java
+++ src/ar/edu/unlam/PBII20212QParcial1Grupo4/PesoArgentino.java
@@ -6,13 +6,26 @@ public class PesoArgentino extends Cuenta{
 	private final static Double MINIMO_DEPOSITO_PARA_INTERES=10000.00; 
 	private final static Double INTERES=1.03;
 	private Double cotizacionDolar;
+	private Double cotizacionBitcoin;
 	
 	public PesoArgentino(Double saldo) {
 		super(saldo);
-		this.cotizacionSobreDolar = 0.01;
+		
 		this.cotizacionDolar = 185.00;
+		this.cotizacionBitcoin = 9000000.00;
 	}
 	
+	public Double getCotizacionDolar() {
+		return cotizacionDolar;
+	}
+
+
+
+	public Double getCotizacionBitcoin() {
+		return cotizacionBitcoin;
+	}
+
+
 	@Override
 	public void depositar(Double monto) {
 		Double resultadoMonto=monto;
