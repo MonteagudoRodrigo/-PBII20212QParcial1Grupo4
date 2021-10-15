1596c03e40a6939463a228e732843ba5978a857c
Tomás Aranda <tomas.aranda.95@gmail.com>
2021-10-15 16:08:38 -0300

    Update BilleteraTest.java

--- test/ar/edu/unlam/PBII20212QParcial1Grupo4/BilleteraTest.java
+++ test/ar/edu/unlam/PBII20212QParcial1Grupo4/BilleteraTest.java
@@ -7,7 +7,7 @@ import org.junit.Test;
 public class BilleteraTest {
 
 	@Test
-	public void queSeAgregueCorrectamente3CuentasDeDistintoTipo() {
+	public void queSePuedaAgregar3CuentasDeDistintoTipo() {
 		Billetera wallet = new Billetera("Juan", "Perez", 12345678);
 		
 		PesoArgentino cuenta1 = new PesoArgentino(0.0);
@@ -25,11 +25,43 @@ public class BilleteraTest {
 	}
 
 	@Test
-	public void queNoSePuedaAgregar2CuentasDelMismoTipo() {
+	public void queNoSePuedaAgregar2CuentasDelTipoPesos() {
 		Billetera wallet = new Billetera("Juan", "Perez", 12345678);
 		
 		PesoArgentino cuenta1 = new PesoArgentino(0.0);
 		PesoArgentino cuenta2 = new PesoArgentino(0.0);
+		
+		wallet.agregarCuenta(cuenta1);
+		wallet.agregarCuenta(cuenta2);
+		
+		Cuenta[] valorEsperado = { cuenta1 };
+		Cuenta[] valorObtenido = wallet.getCuentas();
+		
+		assertArrayEquals(valorEsperado, valorObtenido);
+	}
+
+	@Test
+	public void queNoSePuedaAgregar2CuentasDelTipoDolares() {
+		Billetera wallet = new Billetera("Juan", "Perez", 12345678);
+		
+		DolarEstadounidense cuenta1 = new DolarEstadounidense(0.0);
+		DolarEstadounidense cuenta2 = new DolarEstadounidense(0.0);
+		
+		wallet.agregarCuenta(cuenta1);
+		wallet.agregarCuenta(cuenta2);
+		
+		Cuenta[] valorEsperado = { cuenta1 };
+		Cuenta[] valorObtenido = wallet.getCuentas();
+		
+		assertArrayEquals(valorEsperado, valorObtenido);
+	}
+
+	@Test
+	public void queNoSePuedaAgregar2CuentasDelTipoBitcoin() {
+		Billetera wallet = new Billetera("Juan", "Perez", 12345678);
+		
+		Bitcoin cuenta1 = new Bitcoin(0.0);
+		Bitcoin cuenta2 = new Bitcoin(0.0);
 		
 		wallet.agregarCuenta(cuenta1);
 		wallet.agregarCuenta(cuenta2);
