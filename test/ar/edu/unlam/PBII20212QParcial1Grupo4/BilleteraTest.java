4519d8b6e5a112a7bf23fa93f5081031d34c7e99
Tomás Aranda <tomas.aranda.95@gmail.com>
2021-10-15 16:05:23 -0300

    Agrego Tests de Billetera y refactorizo sus metodos

--- test/ar/edu/unlam/PBII20212QParcial1Grupo4/BilleteraTest.java
+++ test/ar/edu/unlam/PBII20212QParcial1Grupo4/BilleteraTest.java
@@ -7,8 +7,37 @@ import org.junit.Test;
 public class BilleteraTest {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void queSeAgregueCorrectamente3CuentasDeDistintoTipo() {
+		Billetera wallet = new Billetera("Juan", "Perez", 12345678);
+		
+		PesoArgentino cuenta1 = new PesoArgentino(0.0);
+		DolarEstadounidense cuenta2 = new DolarEstadounidense(0.0);
+		Bitcoin cuenta3 = new Bitcoin(0.0);
+		
+		wallet.agregarCuenta(cuenta1);
+		wallet.agregarCuenta(cuenta2);
+		wallet.agregarCuenta(cuenta3);
+		
+		Cuenta[] valorEsperado = { cuenta1, cuenta2, cuenta3 };
+		Cuenta[] valorObtenido = wallet.getCuentas();
+		
+		assertArrayEquals(valorEsperado, valorObtenido);
+	}
+
+	@Test
+	public void queNoSePuedaAgregar2CuentasDelMismoTipo() {
+		Billetera wallet = new Billetera("Juan", "Perez", 12345678);
+		
+		PesoArgentino cuenta1 = new PesoArgentino(0.0);
+		PesoArgentino cuenta2 = new PesoArgentino(0.0);
+		
+		wallet.agregarCuenta(cuenta1);
+		wallet.agregarCuenta(cuenta2);
+		
+		Cuenta[] valorEsperado = { cuenta1 };
+		Cuenta[] valorObtenido = wallet.getCuentas();
+		
+		assertArrayEquals(valorEsperado, valorObtenido);
 	}
 
 }
